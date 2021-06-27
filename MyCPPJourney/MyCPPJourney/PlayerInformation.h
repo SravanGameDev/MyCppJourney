e04f376fa65b0635675a7550fbdb5627ddb49ec9
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-06-27 16:19:57 +0530

    Created a class which stores data of a player profile

---
+++ MyCPPJourney/MyCPPJourney/PlayerInformation.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include <stdio.h>
+using namespace std;
+
+class PlayerInformation
+{
+public:
+
+	string playerName;
+	int yearsPlayed;
+
+	void getdata(void);
+	void displaydata(void);
+};
+
--- MyCPPJourney/MyCPPJourney/Source.cpp
+++ MyCPPJourney/MyCPPJourney/Source.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <stdio.h>
+#include "PlayerInformation.h"
 using namespace std;
 
 int main()
 {
-	//Average of two numbers
-	int a;
-	int b;
-	int average;
-	cout << "Enter a Number\n";
-	cin >>a;
-	cin >> b;
-	average = a + b / 2;
-	cout << "Average is "+ average ;
-	cout << a;
+	PlayerInformation  playerInfo;
+	playerInfo.getdata();
+	playerInfo.displaydata();
 }
 
+void AverageNumber()
+{
+	//Average of two numbers
+	float number1;
+	float number2;
+	float sum;
+	float average;
+
+	cout << "Enter a Number\n";
+	cin >> number1;
+	cin >> number2;
+
+	sum = number1 + number2;
+	average = sum / 2;
+	cout << "Average is " << average << "\n";
+}
