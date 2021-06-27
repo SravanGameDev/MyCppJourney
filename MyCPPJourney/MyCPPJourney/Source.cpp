6e913a83bdd69bc8427d4750a7269e2de2153524
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-06-27 14:34:33 +0530

    Project Created

---
+++ MyCPPJourney/MyCPPJourney/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	//Average of two numbers
+	int a;
+	int b;
+	int average;
+	cout << "Enter a Number\n";
+	cin >>a;
+	cin >> b;
+	average = a + b / 2;
+	cout << "Average is "+ average ;
+	cout << a;
+}
+
