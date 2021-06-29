4a9ff52d79d1a58183464e9e800a4dd096cbc4bc
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-06-29 22:15:09 +0530

    Number Loop

--- MyCPPJourney/MyCPPJourney/PrintLoop.cpp
+++ MyCPPJourney/MyCPPJourney/PrintLoop.cpp
@@ -8,13 +8,23 @@ void PrintLoop::getUserInput(void)
 
 void PrintLoop::displayData(void)
 {
-	for (size_t i = 1; i < number; i++)
+	//for (size_t i = 1; i < number; i++)
+	//{
+	//	//cout << i;
+	//	for (size_t j = 1; j < i; j++)
+	//	{
+	//		cout << i;
+	//	}
+	//	cout << endl;
+	//}
+
+	for (size_t i = 0; i < number; i++)
 	{
-		//cout << i;
-		for (size_t j = 1; j < i; j++)
+		for (size_t j = 0; j < i; j++)
 		{
 			cout << i;
 		}
 		cout << endl;
+		number++;
 	}
 }
