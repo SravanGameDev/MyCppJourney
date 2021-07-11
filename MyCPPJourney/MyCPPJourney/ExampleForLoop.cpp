9ace15072abbc17fceb8b2594c297c00a4553677
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-07-11 18:24:14 +0530

    For Loop
    Question:
    Factorial of a number

--- MyCPPJourney/MyCPPJourney/ExampleForLoop.cpp
+++ MyCPPJourney/MyCPPJourney/ExampleForLoop.cpp
@@ -5,7 +5,26 @@ void ExampleHeader::getUserInputs()
 	Question1();
 }
 
+/// <summary>
+/// Factorial of a number
+/// </summary>
 void ExampleHeader::Question1()
 {
-	std::cout << "Function called";
+	int factorial=1;
+	std::cout << "Enter a number: ";
+	std::cin >> number;
+
+	if (0<=number)
+	{
+		for (int i = 1; i <= number; i++)
+		{
+			factorial = factorial * i;
+		}
+		std::cout << "Factorial of " << number << "! is: " << factorial;
+	}
+	else
+	{
+		std::cout << "Enter a vaild number which greater than zero";
+	}
+
 }
--- MyCPPJourney/MyCPPJourney/ExampleHeader.h
+++ MyCPPJourney/MyCPPJourney/ExampleHeader.h
@@ -4,6 +4,10 @@
 class ExampleHeader
 {
 public:
+	int number;
+
+
+public:
 	void getUserInputs(void);
 	void Question1(void);
 
